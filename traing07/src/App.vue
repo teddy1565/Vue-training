b62447635c31af19e3b691ca4d15fe1083436874
teddy1565 <teddy1565@gmail.com>
2022-02-07 11:56:29 +0800

    v-for

--- traing07/src/App.vue
+++ traing07/src/App.vue
@@ -2,8 +2,8 @@
   <div id="app">
     <h1>To-Do List</h1>
     <ul>
-      <li>
-        <ToDoItem label="GGGGG" v-bind:done="true"/>
+      <li v-for="item in ToDoItems" v-bind:key="item.id">
+        <ToDoItem v-bind:label="item.label" v-bind:done="true"/>
       </li>
     </ul>
   </div>
@@ -11,13 +11,40 @@
 
 <script>
 
-import ToDoItem from "./components/ToDoItem.vue"
+import ToDoItem from "./components/ToDoItem.vue";
+import uniqueId from "lodash.uniqueid";
 
 
 export default {
   name: 'App',
   components: {
     ToDoItem
+  },
+  data() {
+    return {
+      ToDoItems: [
+        {
+          id:uniqueId("todo-"),
+          "label":"Learn Vue",
+          done:false
+        },
+        {
+          id:uniqueId("todo-"),
+          "label":"Create a Vue project with the CLI",
+          done:true
+        },
+        {
+          id:uniqueId("todo-"),
+          "label":"Have fun",
+          done:true
+        },
+        {
+          id:uniqueId("todo-"),
+          "label":"Create a to-do list",
+          done:false
+        }
+      ]
+    };
   }
 }
 </script>
