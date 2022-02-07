f751c7ed7816141d4038b96ec4c49b7d71fc1864
teddy1565 <teddy1565@gmail.com>
2022-02-07 11:49:32 +0800

    v-bind

---
+++ traing07/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <h1>To-Do List</h1>
+    <ul>
+      <li>
+        <ToDoItem label="GGGGG" v-bind:done="true"/>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+import ToDoItem from "./components/ToDoItem.vue"
+
+
+export default {
+  name: 'App',
+  components: {
+    ToDoItem
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
