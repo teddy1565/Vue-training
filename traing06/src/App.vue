ee5952e288a1e48e601fce4e53d0f54986bd8402
teddy1565 <teddy1565@gmail.com>
2022-02-07 11:31:13 +0800

    v-for fail

---
+++ traing06/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul>
+        <li v-for="item in ToDoItems" :key="item.id">
+          <ToDoItem :label="item.label" :done="item.done"/>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+import ToDoItem from "./components/ToDoItem.vue"
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    ToDoItem
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ traing06/src/components/ToDoItem.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <input type="checkbox" :id="id" :checked="isDone" />
+        <label :for="id">{{label}}</label>
+    </div>
+</template>
+<script>
+import uniqueId from "lodash.uniqueid";
+
+export default {
+    name:"ToDoItem",
+    components: {
+        ToDoItem
+    },
+    data() {
+        return {
+            ToDoItems: [
+                {
+                    id:uniqueId("todo-"),
+                    label: "Learn Vue",
+                    done: false
+                },
+                {
+                    id:uniqueId("todo-"),
+                    label: "Create a Vue project with the CLI",
+                    done:true
+                },
+                {
+                    id:uniqueId("todo-"),
+                    label: "Have fun",
+                    done:true
+                },
+                {
+                    id:uniqueId("todo-"),
+                    label: "Create a to-do list",
+                    done:false
+                }
+            ]
+        }
+    }
+}
+</script>
