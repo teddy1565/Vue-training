49105d5f87152c142dd4928109cd12cd38462ff1
teddy1565 <teddy1565@gmail.com>
2022-02-10 17:49:58 +0800

    git test

--- traing08/src/App.vue
+++ traing08/src/App.vue
@@ -1,8 +1,6 @@
 <template>
-  <div>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
